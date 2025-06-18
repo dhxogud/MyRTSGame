a1041b9133aa93fc2eec6bef609527277471e1e2
dhxogud <hyong0117@naver.com>
2025-06-18 11:40:44 +0900

    Initial commit

---
+++ Source/MyRTSGame/InGame/InGame_GM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InGame_GM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYRTSGAME_API AInGame_GM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
